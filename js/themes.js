cfbeb4a0280b905d5130b596770d3e6da1e128f4
Jacob Evan Shreve <jacob@shreve.io>
2020-06-21 15:16:14 -0400

    Update theme to respect browser prefers-color-scheme

--- js/themes.js
+++ js/themes.js
@@ -1,35 +1,68 @@
 let initThemes = () => {
-  if (document.getElementById('dark-mode-link')) return;
+  if (document.getElementById('theme-link')) return;
+
+  let prefers = (query) => {
+    return window.matchMedia('(prefers-color-scheme: ' + query + ')').matches;
+  }
+
+  // Query for the current theme now
+  // default ~= dark
+  let currentTheme = (() => {
+    if (prefers('light')) {
+      return 'light';
+    } else {
+      return 'dark';
+    }
+  })()
+
+  let swapClasses = (prev, next) => {
+    document.body.classList.remove('default-mode');
+    document.body.classList.remove(prev + '-mode');
+    document.body.classList.add(next + '-mode');
+  }
 
   let updateDisplay = () => {
-    if (document.body.classList.contains('dark-mode')) {
-      darkLink.text = "🌜";
+    if (currentTheme == 'dark') {
+      darkLink.text = "🌞";
       darkLink.title = "Switch on Light Mode";
     } else {
-      darkLink.text = "🌞";
+      darkLink.text = "🌜";
       darkLink.title = "Switch on Dark Mode";
     }
   }
 
   let modeToggle = (event) => {
     event.preventDefault();
-    document.body.classList.toggle('dark-mode');
-    let darkMode = document.body.classList.contains('dark-mode');
-    try { sessionStorage.setItem('dark-mode', darkMode); } catch (e) { }
+
+    if (currentTheme == 'dark') {
+      currentTheme = 'light'
+      swapClasses('dark', 'light');
+    } else {
+      currentTheme = 'dark'
+      swapClasses('light', 'dark');
+    }
+
+    try { sessionStorage.setItem('theme', currentTheme); } catch (e) { }
     updateDisplay();
   }
 
   let darkLink = document.createElement('a');
-  darkLink.id = "dark-mode-link"
-  darkLink.title = "Dark Mode";
-  updateDisplay();
+  darkLink.id = "theme-link"
+  darkLink.title = "Toggle Theme";
   darkLink.addEventListener('click', modeToggle);
   document.body.insertBefore(darkLink, document.body.children[0]);
+  updateDisplay();
 
   try {
-    if (sessionStorage.getItem('dark-mode') === "false") {
-      document.body.classList.remove('dark-mode');
+
+    // Ask for a theme preference
+    let theme = sessionStorage.getItem('theme') || 'default'
+    if (theme != 'default') {
+      document.body.classList.add(theme + '-mode');
+      document.body.classList.remove('default-mode');
     }
+
+    // Don't perform the gradual CSS transition on first paint
     setTimeout(() => { document.body.classList.add('transitions') }, 0);
   } catch (e) { }
 
