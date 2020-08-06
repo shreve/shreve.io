13a679fd2fdce61eb5cf9b19fb8666093eacc09b
Jacob Evan Shreve <jacob@shreve.io>
2020-08-06 12:25:25 -0400

    Fix email and front-page grid

--- js/email.js
+++ js/email.js
@@ -10,11 +10,14 @@ let initEmail = () => {
   }
   const ebytes = [2, 21, 23, 31, 17, 122, 92, 71, 1, 13, 4, 0, 88, 12, 65];
 
-  document.getElementById('email').addEventListener('click', (e) => {
-    e.preventDefault();
-    let email = str(byte_xor(ebytes, bytes(location.origin)))
-    window.location = "mailto:" + email;
-  });
+  let emailLink = document.getElementById('email');
+  if (emailLink) {
+    emailLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      let email = str(byte_xor(ebytes, bytes(location.origin)))
+      window.location = "mailto:" + email;
+    });
+  }
 }
 
 export { initEmail };
