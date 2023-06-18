31cac83827aa453aa9ae37868455a0d5b5065555
Violet Shreve <shreve@umich.edu>
2023-06-18 13:43:18 -0400

    Fix email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,25 +1,29 @@
 let initEmail = () => {
   const bytes = (str) => {
-    return str.split('').map((c) => c.charCodeAt(0));
-  }
+    return str.split("").map((c) => c.charCodeAt(0));
+  };
   const str = (bytes) => {
-    return bytes.map((b) => String.fromCharCode(b)).join('');
-  }
+    return bytes.map((b) => String.fromCharCode(b)).join("");
+  };
   const byte_xor = (b1, b2) => {
-    return b1.map((b, i) => { return b ^ b2[i] });
-  }
-  const ebytes = [25,  9, 17, 10, 20, 37, 93, 1,
-                  29, 22, 30, 23, 75, 31, 10];
+    return b1.map((b, i) => {
+      return b ^ b2[i % b2.length];
+    });
+  };
+  const ebytes = [5, 1, 29, 9, 19, 17, 110, 26, 7, 1, 13, 4, 0, 88, 12, 65];
+  str(byte_xor(ebytes, bytes(location.hostname)));
 
-  document.querySelectorAll('a.email').forEach((a) => {
-    a.addEventListener('click', (e) => {
+  document.querySelectorAll("a.email").forEach((a) => {
+    a.addEventListener("click", (e) => {
       e.preventDefault();
       let host = location.hostname;
-      while (host.length < ebytes.length) { host = host + host; }
-      let email = str(byte_xor(ebytes, bytes(host)))
+      while (host.length < ebytes.length) {
+        host = host + host;
+      }
+      let email = str(byte_xor(ebytes, bytes(host)));
       window.location = "mailto:" + email;
     });
   });
-}
+};
 
 export { initEmail };
