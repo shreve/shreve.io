0401585e4e94102fc5b8cff77c993ff0642f7c99
Violet Shreve <shreve@umich.edu>
2023-06-18 20:59:23 -0400

    Use partialCached to fix image processing issue

--- assets/js/images.js
+++ assets/js/images.js
@@ -3,12 +3,13 @@ let initImages = () => {
     let preload = new Image();
     preload.onload = () => {
       image.src = preload.src;
-      image.classList.remove('img-to-load');
-    }
-    preload.src = image.getAttribute('data-src');
-  }
+      image.classList.remove("img-to-load");
+      image.removeAttribute("data-src");
+    };
+    preload.src = image.getAttribute("data-src");
+  };
 
-  for (let image of document.getElementsByClassName('img-to-load')) {
+  for (let image of document.getElementsByClassName("img-to-load")) {
     preloadImage(image);
   }
 };
